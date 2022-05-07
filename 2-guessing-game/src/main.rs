bd3717cf440c63aa164e7fe4d5b907316ead4a0f
Sigurd Tullander <legotull.99@gmail.com>
2022-05-07 11:36:34 +0200

    Finish chapter 2

--- 2-guessing-game/src/main.rs
+++ 2-guessing-game/src/main.rs
@@ -1,15 +1,32 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Gjett tallet");
 
-    println!("Vennligst skriv inn gjettet ditt");
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    loop {
+        println!("Vennligst skriv inn gjettet ditt");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Klarte ikke lese inn det du skrev");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Klarte ikke lese inn det du skrev");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("Du gjetta {}", guess);
+        println!("Du gjetta {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Større"),
+            Ordering::Greater => println!("Mindre"),
+            Ordering::Equal => {
+                println!("Riktig!");
+                break;
+            }
+        }
+    }
 }
